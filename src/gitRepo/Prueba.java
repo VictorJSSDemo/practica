df4a867781cd3df5bff64d90ebc9db62ba936f96
Victor Jimenez <Victorjss96@gmail.com>
2021-12-26 19:45:00 +0100

    commit

---
+++ src/gitRepo/Prueba.java
@@ -0,0 +1,15 @@
+package gitRepo;
+
+public class Prueba {
+	
+private final String message;
+
+public Prueba() {
+	this.message = "Hola mundo";
+}
+	
+public String getMessage() {
+	return message;
+}
+}
+
